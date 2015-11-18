270c750d24eca77fe6b86cdc48a5c546629dbe68
alpenliebe <alpseinstein@gmail.com>
2015-11-18 18:50:47 -0500

    New apis for cordova-muse js interface

--- www/muse.js
+++ www/muse.js
@@ -4,132 +4,132 @@ var argscheck = require('cordova/argscheck'),
     exec = require('cordova/exec'),
     cordova = require('cordova');
 
+var PLUGIN_NAME = "Muse";
+
 function Muse() {}
 
 Muse.prototype.getMuseList = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getMuseList", []);
+    exec(successCallback, errorCallback, PLUGIN_NAME, "getMuseList", []);
 }
-
 Muse.prototype.connectToMuse = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "connectToMuse", []);
+    exec(successCallback, errorCallback, PLUGIN_NAME, "connectToMuse", []);
 }
-
+Muse.prototype.getConnectionState = function(successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "getConnectionState", []);
+}
 Muse.prototype.disconnectMuse = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "disconnectMuse", []);
+    exec(successCallback, errorCallback, PLUGIN_NAME, "disconnectMuse", []);
 }
-
 Muse.prototype.startRecording = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "startRecording", []);
+    exec(successCallback, errorCallback, PLUGIN_NAME, "startRecording", []);
 }
-
+Muse.prototype.setWatch = function(successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "setWatch", []);
+}
 Muse.prototype.stopRecording = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "stopRecording", []);
+    exec(successCallback, errorCallback, PLUGIN_NAME, "stopRecording", []);
+}
+Muse.prototype.getBlink = function(successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "getBlink", []);
+}
+Muse.prototype.testConnection = function(successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "testConnection", []);
 }
 
-Muse.prototype.getAccLeftRight = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getAccLeftRight", []);
+/*Muse.prototype.getAccLeftRight = function(successCallback, errorCallback) {
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getAccLeftRight", []);
 }
 
 Muse.prototype.getAccForwardBackward = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getAccForwardBackward", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getAccForwardBackward", []);
 }
 
 Muse.prototype.getAccUpDown = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getAccUpDown", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getAccUpDown", []);
 }
 
 Muse.prototype.getEegLeftEar = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getEegLeftEar", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getEegLeftEar", []);
 }
 
 Muse.prototype.getEegRightEar = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getEegRightEar", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getEegRightEar", []);
 }
 
 Muse.prototype.getEegLeftForehead = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getEegLeftForehead", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getEegLeftForehead", []);
 }
 
 Muse.prototype.getEegRightForehead = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getEegRightForehead", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getEegRightForehead", []);
 }
 
 Muse.prototype.getAlphaRelativeChannel1 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getAlphaRelativeChannel1", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getAlphaRelativeChannel1", []);
 }
 
 Muse.prototype.getAlphaRelativeChannel2 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getAlphaRelativeChannel2", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getAlphaRelativeChannel2", []);
 }
 
 Muse.prototype.getAlphaRelativeChannel3 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getAlphaRelativeChannel3", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getAlphaRelativeChannel3", []);
 }
 
 Muse.prototype.getAlphaRelativeChannel4 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getAlphaRelativeChannel4", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getAlphaRelativeChannel4", []);
 }
 
 Muse.prototype.getBetaRelativeChannel1 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getBetaRelativeChannel1", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getBetaRelativeChannel1", []);
 }
 
 Muse.prototype.getBetaRelativeChannel2 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getBetaRelativeChannel2", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getBetaRelativeChannel2", []);
 }
 
 Muse.prototype.getBetaRelativeChannel3 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getBetaRelativeChannel3", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getBetaRelativeChannel3", []);
 }
 
 Muse.prototype.getBetaRelativeChannel4 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getBetaRelativeChannel4", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getBetaRelativeChannel4", []);
 }
 
 Muse.prototype.getThetaRelativeChannel1 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getThetaRelativeChannel1", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getThetaRelativeChannel1", []);
 }
 
 Muse.prototype.getThetaRelativeChannel2 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getThetaRelativeChannel2", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getThetaRelativeChannel2", []);
 }
 
 Muse.prototype.getThetaRelativeChannel3 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getThetaRelativeChannel3", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getThetaRelativeChannel3", []);
 }
 
 Muse.prototype.getThetaRelativeChannel4 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getThetaRelativeChannel4", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getThetaRelativeChannel4", []);
 }
 
 Muse.prototype.getGammaRelativeChannel1 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getGammaRelativeChannel1", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getGammaRelativeChannel1", []);
 }
 
 Muse.prototype.getGammaRelativeChannel2 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getGammaRelativeChannel2", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getGammaRelativeChannel2", []);
 }
 
 Muse.prototype.getGammaRelativeChannel3 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getGammaRelativeChannel3", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getGammaRelativeChannel3", []);
 }
 
 Muse.prototype.getGammaRelativeChannel4 = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getGammaRelativeChannel4", []);
-}
-
-Muse.prototype.getBlink = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getBlink", []);
-}
-
-Muse.prototype.testConnection = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "testConnection", []);
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getGammaRelativeChannel4", []);
 }
 
 Muse.prototype.getRecordingData = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Muse", "getRecordingData", []);
-}
-
-
+  exec(successCallback, errorCallback, PLUGIN_NAME, "getRecordingData", []);
+}*/
 
 module.exports = new Muse();
